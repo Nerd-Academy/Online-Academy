2eed721e8eb4d176e83c3ba328e4ca076b6797fc
OdehYazan <eng.yazanodeh@gmail.com>
2021-05-23 18:58:54 +0300

    home-imgs-css

--- js/join.js
+++ js/join.js
@@ -1,28 +1,26 @@
 'use strict'
-let user=[];
+let user = [];
 
 
 
 
- function User(firstname,lastname,email,major)
- {
-     this.firstname=firstname;
-     this.lastname=lastname;
-     this.email=email;
-     this.major=major;
-     user.push(this);
-    
- }
-let formEL=document.getElementById('JoinUs') ;
-formEL.addEventListener('submit',Adduser);
-function Adduser(event)
-{
+function User(firstname, lastname, email, major) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.email = email;
+    this.major = major;
+    user.push(this);
+
+}
+let formEL = document.getElementById('JoinUs');
+formEL.addEventListener('submit', Adduser);
+function Adduser(event) {
     event.preventDefault();
-    let newUserFirstName=event.target.firstname.value;
-    let newUseLastName=event.target.lastname.value;
-    let newUserEmail=event.target.email.value;
-    let newUserMajor=event.target.major.value;
+    let newUserFirstName = event.target.firstname.value;
+    let newUseLastName = event.target.lastname.value;
+    let newUserEmail = event.target.email.value;
+    let newUserMajor = event.target.major.value;
 
-    let newUser=new User(newUserFirstName,newUseLastName,newUserEmail,newUserMajor);
-   console.log(newUser);
-} 
+    let newUser = new User(newUserFirstName, newUseLastName, newUserEmail, newUserMajor);
+    console.log(newUser);
+}
